3370053546ed40fd06738e4602d5e9792bde091c
Joseph Belmonte <joseph.belmonte@gmail.com>
2023-10-04 09:18:59 -0400

    Added release notes to readme, error logging when copy fails

--- scripts/content.js
+++ scripts/content.js
@@ -4,7 +4,9 @@ const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.target.id === "glance_ssn_key") {
       const sessionKey = mutation.target.outerText;
-      navigator.clipboard.writeText(sessionKey);
+      navigator.clipboard.writeText(sessionKey).catch((error) => {
+        console.log("Session key copy failed: ", error);
+      });
     }
   }
 };
